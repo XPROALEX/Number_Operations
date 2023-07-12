96e00e2984aaa0a45fc2a73bca8820a6ba8e3288
Alex <alex97av@gmail.com>
2023-07-12 16:46:45 +0200

    aggiunta stringhe (valore b e valore moltiplicazione)

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         int b =a;
         b ++;
         boolean isOdd= b % 2 != 0;
+        System.out.println("B value="+b);
         System.out.println("Is B an odd number? "+isOdd);
         int multiply=b*(b+1);
         boolean isMultiple=multiply % 3 == 0;
+        System.out.println("multiply the value of b by its own value+1 = "+multiply);
         System.out.println("Is the result multiple of 3? "+ isMultiple);
     }
 }
