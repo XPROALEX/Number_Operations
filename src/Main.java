ce3d95b2c96e8cceb56762deeb67b4a0ba78e86b
Alessandro <alex97av@gmail.com>
2023-07-18 18:56:56 +0200

    correzione esercizio

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         boolean isOdd= b % 2 != 0;
         int multiply=b*(b+1);
         boolean isMultiple=multiply % 3 == 0;
-        System.out.println("B value="+b+"\nIs B an odd number? "+isOdd+"\nmultiply the value of b by its own value+1 = "+multiply+"\nIs the result multiple of 3? "+ isMultiple);
+        System.out.println(isOdd&&isMultiple);
+        //System.out.println("B value="+b+"\nIs B an odd number? "+isOdd+"\nmultiply the value of b by its own value+1 = "+multiply+"\nIs the result multiple of 3? "+ isMultiple);
     }
 }
